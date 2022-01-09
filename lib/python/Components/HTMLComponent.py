c1c4e0d6637fbfadfcbbd7ea98c62d6879c7de4f
Boris <boris.fox.solovyev@gmail.com>
2022-01-09 09:09:55 +0200

    del

--- lib/python/Components/HTMLComponent.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Just here for old plugins that import it
-class HTMLComponent:
-	pass
--- lib/python/Screens/SubtitleDisplay.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from Screens.Screen import Screen
-
-
-class SubtitleDisplay(Screen):
-	pass
-
-	# not really much to do...
